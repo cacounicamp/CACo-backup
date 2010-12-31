24b46011f14eec170ad394ebe3980716f24dedfb
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2010-12-31 15:56:24 -0200

    Refatorando tudo em um arquivo só

--- backup.py
+++ backup.py
@@ -18,23 +18,75 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# standard libs
+################################################################################
+# Imports
+################################################################################
+
 import os
 import ConfigParser
 import logging
 
-# libs locais
-import utils
+################################################################################
+# Configurando o logging
+################################################################################
 
-# configurando o logging
-# TODO: configurar melhor a saída do log
-LOG_FILE = "${HOME}/caco_backup.log"
-logging.basicConfig(filename=os.path.expandvars(LOG_FILE))
+log_file = os.path.expandvars("${HOME}/caco_backup.log")
+if os.path.lexists(log_file):
+    os.remove(log_file) # limpando o log antigo
+logging.basicConfig(filename=log_file,
+                    format="%(asctime)s: %(name)s:%(levelname)s %(message)s")
 logger = logging.getLogger("backup.py")
 logger.setLevel(logging.DEBUG)
 
+################################################################################
+# Classes
+################################################################################
+
+class Tools():
+    # IMPORTANTE: para funcionar, precisa ter chave ssh e host do destino no
+    # know_hosts do usuário
+    def rsync(self, source, destinyURL, opts=""):
+        """ Executa um rsync. O argumento 'opts' é opcional """
+        if (type(source) != type(str()) or
+            type(destinyURL) != type(str()) or
+            type(opts) != type(str())):
+            logger.error("Argumentos de 'rsync' não são strings")
+            return os.EX_DATAERR
+        # rsync -avLz
+        rsync_command="rsync --archive --verbose --copy-links --compress"
+        # executa o comando e guarda o status de retorno
+        exit_status = os.system(" ".join([rsync_command, opts, source, destinyURL]))
+        if exit_status != os.EX_OK:
+            logger.error("Erro durante o rsync: " + format(exit_status))
+        else:
+            logger.info("Rsync de " + source + " para " + destinyURL+ " executado com sucesso")
+        return exit_status
+    def compress(self, compression_type, source, opts=""):
+        """ Compacta um arquivo/diretório em algum lugar em /tmp, retonando o
+        caminho desse novo arquivo """
+        if (type(compression_type) != type(str()) or
+            type(source) != type(str()) or
+            type(opts) != type(str())):
+            logger.error("Argumentos de 'compress' não são strings")
+            return os.EX_DATAERR
+
+        # dicionário com os tipos de compressão e seus respectivos comandos
+        compression_opts = {
+            "xz":"tar -cJf",
+            "gz":"tar -czf",
+            "bz2": "tar -cjf"
+        }
+        tmp_file = os.tmpnam() + ".tar." + compression_type
+        exit_status = os.system(" ".join([compression_opts[compression_type], opts, tmp_file, source]))
+        if exit_status != os.EX_OK:
+            logger.warning("Erro durante a compressão do arquivo: " + format(exit_status))
+            tmp_file = "" # retornará string vazia para o caminho em caso de falha
+        else:
+            logger.info("Compressão de " + source + " realizada com sucesso")
+        return tmp_file
+
 # Classe que representa um alvo de backup
-class BackupTarget():
+class BackupTarget(Tools):
     def __init__(self, source, destiny, protocol_type, compress_type = "xz"):
         # FIXME: encontrar uma forma mais elegante para validação dos parâmetros, e aplicar onde for necessário
         if (type(source) != type(str()) or
@@ -51,25 +103,39 @@ class BackupTarget():
     def run_backup(self):
         """ Realiza as operações de backup """
         if self.protocol == "rsync":
-            status = utils.rsync(self.source_path, self.destiny)
+            status = self.rsync(self.source_path, self.destiny)
         else:
             logger.warning("Protocolo especificado é inexistente")
             status = os.EX_DATAERR
         return status
 
-    # TODO: testar a função pack
     def pack(self):
         """
          Compacta o arquivo/diretório do alvo de backup. O novo alvo passa a
          ser um arquivo compactado em /tmp
         """
-        new_path = utils.compress(self.compress, self.source_path)
+        new_path = self.compress(self.compress, self.source_path)
         if new_path != "":
             self.source_path = new_path
         else:
             logger.error("Erro na compactação do arquivo, utilizando arquivo original")
 
-### iniciando a execução do script
+################################################################################
+# Funções
+################################################################################
+
+# TODO: testar funcionamento do gerador de lista de pacotes
+def lista_de_pacotes(pkglist_file = "~/.pkg_list"):
+    """ Função que gera a lista dos pacotes instalados no sistema """
+    if type(pkglist_file) != type(str()):
+        logger.error("Argumento de 'listaDePacotes' não é string")
+        return ""
+    os.system("dpkg -l '*' > " + pkglist_file)
+    return pkglist_file
+
+################################################################################
+# Iniciando a execução do script
+################################################################################
 
 # lendo a configuração
 # Exemplo de configuração:
